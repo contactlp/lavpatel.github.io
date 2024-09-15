78a80066a5ea1866cff9aabd5e49514245cc5f28
Lav Patel <8277466+lav-patel@users.noreply.github.com>
2024-09-15 18:50:20 +0000

    github action for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // basePath: "/2048-in-react", --only if you are not using the default repo
   output: "export", // <=== enables static exports
   reactStrictMode: true,
 };
